cb14106608579bffb123afb985a22c06179446b8
CAESARIO <crio.0592@gmail.com>
2016-10-31 13:34:37 +0700

    update

--- template/index/modal/tambah-data.php
+++ template/index/modal/tambah-data.php
@@ -2,45 +2,49 @@
 	<div class="modal-dialog modal-dialog modal-sm">
 		<div class="modal-content">
 			<div class="modal-header">
-				
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h5 class="over-title margin-bottom-0">Tambah <span class="text-bold">Data Akun</span></h5>
 			</div>
 			<div class="modal-body">
-				<div class="col-lg-12 col-md-12">
-					<div class="panel panel-white">
-						<div class="panel-body">
-							<form role="form" class="form-horizontal">
-								<div class="form-group">
-									<label class="col-sm-2 control-label" for="inputEmail3"> No. Akun </label>
-									<div class="col-sm-10">
-										<input type="text" placeholder="Email" id="inputEmail3" class="form-control">
-									</div>
-								</div>
-								<div class="form-group">
-									<label class="col-sm-2 control-label" for="inputPassword3"> Nama Akun </label>
-									<div class="col-sm-10">
-										<input type="text" placeholder="Password" id="inputPassword3" class="form-control">
-									</div>
-								</div>
-								<div class="form-group">
-									<div class="col-sm-offset-2 col-sm-10">
-										<div class="checkbox clip-check check-primary">
-											<input type="checkbox" id="checkbox2" value="1">
-											<label for="checkbox2"> Remember me </label>
+				<div>
+					<div class="col-lg-12 col-md-12">
+						<div class="panel panel-white">
+							<div class="panel-body">
+								<form role="form" class="form-horizontal">
+									<div class="form-group">
+										<label class="col-sm-2 control-label" for="inputEmail3"> No. Akun </label>
+										<div class="col-sm-10">
+											<input type="text" placeholder="Email" id="inputEmail3" class="form-control">
 										</div>
 									</div>
-								</div>
-								<div class="form-group margin-bottom-0">
-									<div class="col-sm-offset-2 col-sm-10">
-										<button class="btn btn-o btn-primary" type="submit">
-											Save
-										</button>
-										<button type="button" class="btn btn-primary" data-dismiss="modal">
-											Close
-										</button>
-				
+									<div class="form-group">
+										<label class="col-sm-2 control-label" for="inputPassword3"> Nama Akun </label>
+										<div class="col-sm-10">
+											<input type="text" placeholder="Password" id="inputPassword3" class="form-control">
+										</div>
 									</div>
-								</div>
-							</form>
+									<div class="form-group">
+										<div class="col-sm-offset-2 col-sm-10">
+											<div class="checkbox clip-check check-primary">
+												<input type="checkbox" id="checkbox2" value="1">
+												<label for="checkbox2"> Remember me </label>
+											</div>
+										</div>
+									</div>
+									<div class="form-group margin-bottom-0">
+										<div class="col-sm-offset-2 col-sm-10">
+											<button class="btn btn-o btn-primary" type="submit">
+												Save
+											</button>
+											<button type="button" class="btn btn-primary" data-dismiss="modal">
+												Close
+											</button>
+										</div>
+									</div>
+								</form>
+							</div>
 						</div>
 					</div>
 				</div>
