f58781d08fa1334bab315ab3f1b880903ed54de0
imanbagus <imanelmessiah@gmail.com>
2016-11-09 14:28:20 +0700

    laporan arus kas

---
+++ template/index/arus_kas.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<!-- Template Name: Packet - Responsive Admin Template build with Twitter Bootstrap 3.x | Author: ClipTheme -->
+<!--[if IE 8]><html class="ie8" lang="en"><![endif]-->
+<!--[if IE 9]><html class="ie9" lang="en"><![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- start: HEAD -->
+<head>
+	<title>Packet - Responsive Admin Template</title>
+	<?php include 'include/main-head.php'; ?>
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" href="assets/css/rio.css">
+</head>
+<!-- end: HEAD -->
+<body>
+<div id="app" class="lyt-2">
+	<!-- sidebar -->
+	<?php include 'include/sidebar.php'; ?>
+	<!-- / sidebar -->
+	<div class="app-content">
+		<!-- start: TOP NAVBAR -->
+		<?php include 'include/nav-header.php'; ?>
+		<!-- end: TOP NAVBAR -->
+		<div class="main-content" >
+			<div class="wrap-content container" id="container">
+				<!-- start: BREADCRUMB -->
+				<div class="breadcrumb-wrapper">
+					<a href="index.php"><h4 class="mainTitle no-margin">Dashboard</h4></a>
+					<h5>Welcome to Beon Accounting</h5>
+				</div>
+				<!-- end: BREADCRUMB -->
+				<!-- start: FIRST SECTION -->
+				
+				<div class="container-fluid padding-bottom-10 padding-top-30">
+					<div class="row margin-bottom">
+						<div class="panel panel-white">
+							<div class="panel-body">
+								<div class="table-responsive">
+
+									<div class="row">
+										<div class="col-md-8 margin-bottom-10">
+											<h3>Laporan Arus KAS</h3>
+										</div>
+										<div class="col-md-4 margin-bottom-10">
+											<label>Sort By :</label>
+											<select name="Tanggal" class="form-control">
+												<option value="">&nbsp;</option>
+												<option value="AL">Alabama</option>
+												<option value="AK">Alaska</option>
+												<option value="AZ">Arizona</option>
+												<option value="AR">Arkansas</option>
+												<option value="CA">California</option>
+												<option value="CO">Colorado</option>
+																		
+											</select>
+										</div>
+										<div class=" pull-right margin-left-5">
+											<p>Rabu, o9-11-2016</p>
+										</div>
+									</div>
+
+
+									<table class="table table-bordered" id="sample-table-1"
+										<thead>
+											<tr class="odd">
+												<th colspan="7" class="text-center">Laporan Arus Kas</th>
+											</tr>
+											<tr>
+												<th>ARUS KAS DARI AKTIFITAS OPERASI</th>
+												<th></th>
+												
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td><input type="text" class="form-control no-border"><b>SUMBER :</b></td>
+												<td><input type="text" class="form-control no-border"></td>
+												
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" value="Piutang Penjualan"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											
+												
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" value="Pembayaran cadangan Kerugian Penjualan"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" value="Refund Biaya Langsung"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" value="Uang Muka Penjualan"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" value="Penyusuaian saldo Bank "></td>
+												<td><input type="text" class="form-control no-border"></td>
+												
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" value="Piutang Lain-lain "></td>
+												<td><input type="text" class="form-control no-border"></td>
+												
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border"><b>PENGGUNAAN<b></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" value="Pembelian TLD RC"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" value="Pembelian ID RC"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" value="Pembelian Domain Webnic "></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border"><b>ARUS KAS BERSIH UNTUK OPERASI</b></td>
+												<td><input type="text" class="form-control no-border"></td>
+												
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border"><b>ARUS KAS DARI AKTIFITAS INVESTASI</b></td>
+												<td><input type="text" class="form-control no-border"></td>	
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border"><b>SUMBER :</b></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border " value="Penjualan AKtiva Tetap"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border"><b>PENGGUNAAN<b></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border " value="Uang Muka Pembelian Aktiva Tetap"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border " value="Pembelian Perangkat(Altive tetap) "></td>
+												<td><input type="text" class="form-control no-border"></td>
+												
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border"><b>ARUS KAS DARI AKTIFITAS PENDANAAN</b></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border"><b>SUMBER :</b></td>
+												<td><input type="text" class="form-control no-border"></td> 
+												
+											</tr>
+											
+											<tr>
+												<td><input type="text" class="form-control no-border" VALUE="Hutang Bank (PRK)"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" VALUE="Hutang Kartu Kredit"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border"><b>PENGGUNAAN<b></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" VALUE="Setoran Dalam Perjalan"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" VALUE="Bon"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" VALUE="Piutang Direksi"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border"><b>ARUS KAS DARI AKTIFITAS PENDANAAN</b></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+												<tr>
+												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+												<tr>
+												<td><input type="text" class="form-control no-border"><b>JUMLAH ARUS KAS BERSIH</b></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+												<tr>
+												<td><input type="text" class="form-control no-border" VALUE="SALDO KAS AWAL"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input type="text" class="form-control no-border" VALUE="SALDO KAS AKHIR"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+
+
+										
+										
+										
+										
+										</tbody>
+									</table>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- start: FOOTER -->
+	<?php include 'include/footer.php'; ?>
+	<!-- end: FOOTER -->
+	<!-- start: OFF-SIDEBAR -->
+	<?php include 'include/off-sidebar.php'; ?>
+	<!-- end: OFF-SIDEBAR -->
+	<!-- start: SETTINGS -->
+	<?php include 'include/settings-panel.php'; ?>
+	<!-- end: SETTINGS -->
+</div>
+<!-- start: MAIN JAVASCRIPTS -->
+<?php include 'include/main-js.php'; ?>
+<!-- start: JavaScript Event Handlers for this page -->
+<script src="assets/js/index.js"></script>
+<script>
+	jQuery(document).ready(function() {
+		Main.init();
+		Index.init();
+	});
+</script>
+<script>
+	var textAreas = document.getElementsByTagName('textarea');
+
+	Array.prototype.forEach.call(textAreas, function(elem) {
+		elem.placeholder = elem.placeholder.replace(/\\n/g, '\n');
+	});
+</script>
+<!-- end: JavaScript Event Handlers for this page -->
+</body>
+</html>
