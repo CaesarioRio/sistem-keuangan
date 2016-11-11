5de9899b1f142ee895d93317c6b062beccb8d9b6
CAESARIO <crio.0592@gmail.com>
2016-11-11 14:09:42 +0700

    update

--- template/index/neraca.php
+++ template/index/neraca.php
@@ -70,11 +70,407 @@
 					<div class="row margin-bottom">
 						<div class="panel panel-white">
 							<div class="panel-body">
-								<div class="table-responsive">
+								<div class="table-responsive margin-bottom-25">
 									<div class="row">
 										<div class="pull-right margin-bottom-5 padding-right-20">
 											<a class="btn btn-primary btn-o btn-wide tombol">Export </a>
 											<i class="fa fa-calendar padding-left-20"></i> Senin, 10 November 2015
+										</div>
+									</div>
+									<table class="table table-bordered" id="sample-table-1">
+										<thead>
+											<tr>
+												<th colspan="4" class="paddings">Aktiva</th>
+												<th colspan="4" class="paddings text-right padding-right-10">Kewajiban dan Ekuitas</th>
+											</tr>
+											<tr class="paddings odd">
+												<th class="paddings">Aktiva Lancar</th>
+												<th class="paddings">Cat</th>
+												<th class="paddings">Bulan Ini</th>
+												<th class="paddings">Bulan Lalu</th>
+												<th class="paddings">Kewajiban Lancar</th>
+												<th class="paddings">Cat</th>
+												<th class="paddings">Bulan Ini</th>
+												<th class="paddings">Bulan Lalu</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td class="paddings text-bold">Jumlah Aktiva Lancar</td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td class="paddings text-bold">Jumlah Kewajiban Lancar</td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr class="paddings odd">
+												<th class="paddings">Aktiva Tetap</th>
+												<th class="paddings"></th>
+												<th class="paddings"></th>
+												<th class="paddings"></th>
+												<th class="paddings">Ekuitas</th>
+												<th class="paddings"></th>
+												<th class="paddings"></th>
+												<th class="paddings"></th>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td class="paddings text-bold">Aktiva Tetap Bersih</td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td class="paddings text-bold">Jumlah Ekuitas</td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td class="paddings text-bold">Aktiva Lain-lain</td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td class="paddings text-bold">Total Aktiva</td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td class="paddings text-bold">Total Kewajiban dan Ekuitas</td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+								<div class="table-responsive margin-bottom-25">
+									<div class="row">
+										<div class="pull-right margin-bottom-5 padding-right-20">
+											<a class="btn btn-primary btn-o btn-wide tombol">Export </a>
+											<i class="fa fa-calendar padding-left-20"></i> Senin, 11 November 2015
+										</div>
+									</div>
+									<table class="table table-bordered" id="sample-table-1">
+										<thead>
+											<tr>
+												<th colspan="4" class="paddings">Aktiva</th>
+												<th colspan="4" class="paddings text-right padding-right-10">Kewajiban dan Ekuitas</th>
+											</tr>
+											<tr class="paddings odd">
+												<th class="paddings">Aktiva Lancar</th>
+												<th class="paddings">Cat</th>
+												<th class="paddings">Bulan Ini</th>
+												<th class="paddings">Bulan Lalu</th>
+												<th class="paddings">Kewajiban Lancar</th>
+												<th class="paddings">Cat</th>
+												<th class="paddings">Bulan Ini</th>
+												<th class="paddings">Bulan Lalu</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td class="paddings text-bold">Jumlah Aktiva Lancar</td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td class="paddings text-bold">Jumlah Kewajiban Lancar</td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr class="paddings odd">
+												<th class="paddings">Aktiva Tetap</th>
+												<th class="paddings"></th>
+												<th class="paddings"></th>
+												<th class="paddings"></th>
+												<th class="paddings">Ekuitas</th>
+												<th class="paddings"></th>
+												<th class="paddings"></th>
+												<th class="paddings"></th>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td class="paddings text-bold">Aktiva Tetap Bersih</td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td class="paddings text-bold">Jumlah Ekuitas</td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td class="paddings text-bold">Aktiva Lain-lain</td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td class="paddings text-bold">Total Aktiva</td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td class="paddings text-bold">Total Kewajiban dan Ekuitas</td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+								<div class="table-responsive margin-bottom-25">
+									<div class="row">
+										<div class="pull-right margin-bottom-5 padding-right-20">
+											<a class="btn btn-primary btn-o btn-wide tombol">Export </a>
+											<i class="fa fa-calendar padding-left-20"></i> Senin, 12 November 2015
 										</div>
 									</div>
 									<table class="table table-bordered" id="sample-table-1">
