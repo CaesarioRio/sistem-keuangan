a76b344998ba290b06fafe04953442f8b7125738
CAESARIO <crio.0592@gmail.com>
2016-11-23 15:25:33 +0700

    update

---
+++ template/index/modal/import-kh.php
@@ -0,0 +1,26 @@
+<div class="modal fade modal-aside vertical top modal-import-kh modal-custom"  tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel3" aria-hidden="true">
+	<div class="modal-dialog modal-dialog modal-sm">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h5 class="over-title margin-bottom-0"> Import <span class="text-bold">Kas Harian</span></h5>
+			</div>
+			<div class="modal-body">
+				<div class="text-center grey-icon">
+					<i class="fa fa-file-excel-o size-60"></i>		
+					<h3 class="grey-icon margin-top-10">Drag an CSV File Here</h3>									
+					<span class="btn btn-info fileinput-button"> <i class="fa fa-upload"></i> <span>Choose your files</span>
+						<input type="file" name="files[]" multiple="">
+					</span>
+				</div>
+			</div>
+			<div class="modal-footer no-border">
+				<button type="button" class="btn btn-primary btn-o" data-dismiss="modal">
+					Close
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
