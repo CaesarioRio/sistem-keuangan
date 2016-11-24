11f7e3c43c833bd82501161d9536876fd3a4d0b0
Ariq Nugraha <mochariqnugraha@yahoo.co.id>
2016-11-24 15:30:34 +0700

    import

--- template/index/input_KH.php
+++ template/index/input_KH.php
@@ -9,7 +9,30 @@
 <head>
 	<title>Packet - Responsive Admin Template</title>
 	<?php include 'include/main-head.php'; ?>
+		<!-- start: MAIN CSS -->
+		<link rel="stylesheet" href="../../bower_components/bootstrap/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="../../bower_components/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="../../bower_components/themify-icons/themify-icons.css">
+		<link rel="stylesheet" href="../../bower_components/flag-icon-css/css/flag-icon.min.css">
+		<link rel="stylesheet" href="../../bower_components/animate.css/animate.min.css">
+		<link rel="stylesheet" href="../../bower_components/perfect-scrollbar/css/perfect-scrollbar.min.css">
+		<link rel="stylesheet" href="../../bower_components/switchery/dist/switchery.min.css">
+		<link rel="stylesheet" href="../../bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css">
+		<link rel="stylesheet" href="../../bower_components/ladda/dist/ladda-themeless.min.css">
+		<link rel="stylesheet" href="../../bower_components/slick.js/slick/slick.css">
+		<link rel="stylesheet" href="../../bower_components/slick.js/slick/slick-theme.css">
+		<!-- end: MAIN CSS -->
+		<!-- start: CSS REQUIRED FOR THIS PAGE ONLY -->
+		<link href="../../bower_components/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
+		<link href="../../bower_components/select2/dist/css/select2.min.css" rel="stylesheet" media="screen">
+		<link href="../../bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
+		<!-- end: CSS REQUIRED FOR THIS PAGE ONLY -->
 	<!-- Favicon -->
+	<!-- start: Packet CSS -->
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/plugins.css">
+		<link rel="stylesheet" href="assets/css/themes/lyt2-theme-1.css" id="skin_color">
+		<!-- end: Packet CSS -->
 	<link rel="shortcut icon" href="favicon.ico" />
 	<link rel="stylesheet" href="assets/css/rio.css">
 </head>
@@ -35,6 +58,7 @@
 				
 				<div class="container-fluid padding-left-30 padding-right-30 padding-top-30">
 					<div class="row margin-bottom">
+						<h5 id="tes"></h5>
 						<div class="panel panel-white">
 							<div class="panel-body">
 								<div class="table-responsive">
@@ -55,16 +79,7 @@
 										</thead>
 										<tbody>
 											<tr>
-												<td><input type="text" class="form-control no-border"></td>
-												<td><input type="text" class="form-control no-border"></td>
-												<td><input type="text" class="form-control no-border"></td>
-												<td><input type="text" class="form-control no-border"></td>
-												<td><input type="text" class="form-control no-border"></td>
-												<td><input type="text" class="form-control no-border"></td>
-												<td><input type="text" class="form-control no-border"></td>
-											</tr>
-											<tr>
-												<td><input type="text" class="form-control no-border"></td>
+												<td><input id="form-field-mask-1" class="form-control input-mask-date no-border" type="text"></td>
 												<td><input type="text" class="form-control no-border"></td>
 												<td><input type="text" class="form-control no-border"></td>
 												<td><input type="text" class="form-control no-border"></td>
@@ -73,16 +88,7 @@
 												<td><input type="text" class="form-control no-border"></td>
 											</tr>
 											<tr>
-												<td><input type="text" class="form-control no-border"></td>
-												<td><input type="text" class="form-control no-border"></td>
-												<td><input type="text" class="form-control no-border"></td>
-												<td><input type="text" class="form-control no-border"></td>
-												<td><input type="text" class="form-control no-border"></td>
-												<td><input type="text" class="form-control no-border"></td>
-												<td><input type="text" class="form-control no-border"></td>
-											</tr>
-											<tr>
-												<td><input type="text" class="form-control no-border"></td>
+												<td><input id="form-field-mask-1" class="form-control input-mask-date no-border" type="text"></td>
 												<td><input type="text" class="form-control no-border"></td>
 												<td><input type="text" class="form-control no-border"></td>
 												<td><input type="text" class="form-control no-border"></td>
@@ -91,7 +97,25 @@
 												<td><input type="text" class="form-control no-border"></td>
 											</tr>
 											<tr>
+												<td><input id="form-field-mask-1" class="form-control input-mask-date no-border" type="text"></td>
 												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input id="form-field-mask-1" class="form-control input-mask-date no-border" type="text"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+												<td><input type="text" class="form-control no-border"></td>
+											</tr>
+											<tr>
+												<td><input id="form-field-mask-1" class="form-control input-mask-date no-border" type="text"></td>
 												<td><input type="text" class="form-control no-border"></td>
 												<td><input type="text" class="form-control no-border"></td>
 												<td><input type="text" class="form-control no-border"></td>
@@ -171,12 +195,72 @@
 </div>
 <!-- start: MAIN JAVASCRIPTS -->
 <?php include 'include/main-js.php'; ?>
+<!-- start: MAIN JAVASCRIPTS -->
+		<script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+		<script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="../../bower_components/components-modernizr/modernizr.js"></script>
+		<script src="../../bower_components/js-cookie/src/js.cookie.js"></script>
+		<script src="../../bower_components/perfect-scrollbar/js/perfect-scrollbar.jquery.min.js"></script>
+		<script src="../../bower_components/jquery-fullscreen/jquery.fullscreen-min.js"></script>
+		<script src="../../bower_components/switchery/dist/switchery.min.js"></script>
+		<script src="../../bower_components/jquery.knobe/dist/jquery.knob.min.js"></script>
+		<script src="../../bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js"></script>
+		<script src="../../bower_components/slick.js/slick/slick.min.js"></script>
+		<script src="../../bower_components/jquery-numerator/jquery-numerator.js"></script>
+		<script src="../../bower_components/ladda/dist/spin.min.js"></script>
+		<script src="../../bower_components/ladda/dist/ladda.min.js"></script>
+		<script src="../../bower_components/ladda/dist/ladda.jquery.min.js"></script>
+		<!-- end: MAIN JAVASCRIPTS -->
+<!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		<script src="../../bower_components/jquery.maskedinput/dist/jquery.maskedinput.min.js"></script>
+		<script src="../../bower_components/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js"></script>
+		<script src="../../bower_components/autosize/dist/autosize.min.js"></script>
+		<script src="../../bower_components/select2/dist/js/select2.min.js"></script>
+		<script src="../../bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+		<script src="../../bower_components/bootstrap-timepicker/js/bootstrap-timepicker.js"></script>
+		<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		<!-- start: Packet JAVASCRIPTS -->
+		<script src="assets/js/letter-icons.js"></script>
+		<script src="assets/js/main.js"></script>
+		<script src="assets/js/moment.js"></script>
+		<!-- end: Packet JAVASCRIPTS -->
+		<!-- start: JavaScript Event Handlers for this page -->
+		<script src="assets/js/selectFx/classie.js"></script>
+		<script src="assets/js/selectFx/selectFx.js"></script>
+		<script src="assets/js/form-elements.js"></script>
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				FormElements.init();
+			});
+		</script>
+		<!-- end: JavaScript Event Handlers for this page --><!-- start: Packet JAVASCRIPTS -->
+		<script src="assets/js/letter-icons.js"></script>
+		<script src="assets/js/main.js"></script>
+		<!-- end: Packet JAVASCRIPTS -->
+		<!-- start: JavaScript Event Handlers for this page -->
+		<script src="assets/js/selectFx/classie.js"></script>
+		<script src="assets/js/selectFx/selectFx.js"></script>
+		<script src="assets/js/form-elements.js"></script>
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				FormElements.init();
+			});
+		</script>
+		<!-- end: JavaScript Event Handlers for this page -->
 <!-- start: JavaScript Event Handlers for this page -->
 <script src="assets/js/index.js"></script>
 <script>
 	jQuery(document).ready(function() {
 		Main.init();
 		Index.init();
+	});
+</script>
+<script>
+$(document).ready(function(){
+	var dt = moment().format('DD/MM/YYYY');
+	$('.input-mask-date').val(dt);
 	});
 </script>
 <script>
